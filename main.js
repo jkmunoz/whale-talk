e71060e798531a039f689bd9d67500241a14d15a
jkmunoz <juliannakmunoz@gmail.com>
2023-08-22 21:49:43 -0500

    fix resultArray.push() to include all necessary vowels.

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-let input = ("There you are. I've been looking for you.");
+//My phrase in english.
+const input = "There you are. I've been looking for you.";
 
+//array of vowels
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
-let resultArray = [];
+//empty array to store new array of whale language.
+const resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-    const inputLetters = input[i];
+    const inputLetters = input[i].toLowerCase();
 
     if (inputLetters === 'e') {
-        resultArray.push(input[i]);
+        resultArray.push(inputLetters);
     }
 
     if (inputLetters === 'u') {
-        resultArray.push(input[i]);
+        resultArray.push(inputLetters);
     }
 
     // console.log(letters.length);
@@ -20,7 +23,7 @@ for (let i = 0; i < input.length; i++) {
         const vowelLetters = vowels[j];
  
         if (inputLetters === vowelLetters) {
-            console.log(inputLetters);
+            resultArray.push(inputLetters);
         }
     }
 }
